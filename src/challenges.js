5afb29270f311aa5e3e6b9103b586b5d83f3f268
Matheus <kamaitatsu@gmail.com>
2021-09-06 19:53:48 -0300

    eslint-enjoado

--- src/challenges.js
+++ src/challenges.js
@@ -1,150 +1,150 @@
 // Desafio 1
 function compareTrue(parametro1, parametro2) {
-     if(parametro1&&parametro2){
-        return true;
-    }else{
-        return false;
-    }
+  if (parametro1 && parametro2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  let area = (base*height)/2;
-return area;
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 
 }
 
 // Desafio 3
 function splitSentence(stringSentence) {
-    let matriz = []
-    matriz = stringSentence.split(' ');
+  let matriz = []
+  matriz = stringSentence.split(' ');
 
-    return matriz;
+  return matriz;
 }
 
 // Desafio 4
 function concatName(arrayConcatName) {
- let newArrayConcatName='';
-    for(let i=0; i<arrayConcatName.length;i++){
-        if(i===0){
-            newArrayConcatName=newArrayConcatName+(arrayConcatName[arrayConcatName.length-1]);
-        }else
-        if((arrayConcatName.length-1)===i){
-            newArrayConcatName=newArrayConcatName+', '+(arrayConcatName[0]);
-        }
-    }
-    
-    return newArrayConcatName;
+  let newArrayConcatName = '';
+  for (let i = 0; i < arrayConcatName.length; i++) {
+    if (i === 0) {
+      newArrayConcatName = newArrayConcatName + (arrayConcatName[arrayConcatName.length - 1]);
+    } else
+      if ((arrayConcatName.length - 1) === i) {
+        newArrayConcatName = newArrayConcatName + ', ' + (arrayConcatName[0]);
+      }
+  }
+
+  return newArrayConcatName;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-    let pontosV = wins*3;
-    let pontosT = ties;
-    let pontos = pontosT+pontosV;
+function footballPoints(wins, ties) {
+  let pontosV = wins * 3;
+  let pontosT = ties;
+  let pontos = pontosT + pontosV;
 
-    return pontos;
+  return pontos;
 }
 
 // Desafio 6
 function highestCount(arrayDeNumeros) {
-	let maior=arrayDeNumeros[0];
-	let vezesMaior=0;
-    for(let i=0;i<arrayDeNumeros.length;i++){
-        if(arrayDeNumeros[i]>=maior){
-            maior = arrayDeNumeros[i];
-        }
+  let maior = arrayDeNumeros[0];
+  let vezesMaior = 0;
+  for (let i = 0; i < arrayDeNumeros.length; i++) {
+    if (arrayDeNumeros[i] >= maior) {
+      maior = arrayDeNumeros[i];
     }
-    for(let i=0;i<arrayDeNumeros.length;i++){
-        if(arrayDeNumeros[i]===maior){
-            vezesMaior+=1;
-        }
+  }
+  for (let i = 0; i < arrayDeNumeros.length; i++) {
+    if (arrayDeNumeros[i] === maior) {
+      vezesMaior += 1;
     }
-    return vezesMaior;
+  }
+  return vezesMaior;
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
-  if(cat2-mouse>cat1-mouse && cat2-mouse>mouse-cat1){
-        return "cat1";
-    }else if(cat1-mouse>cat2>mouse){
-        return "cat2";
-    }else{
-        return "os gatos trombam e o rato foge";
-    }
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat2 - mouse > cat1 - mouse && cat2 - mouse > mouse - cat1) {
+    return "cat1";
+  } else if (cat1 - mouse > cat2 > mouse) {
+    return "cat2";
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
 function fizzBuzz(arrayFizzBuzz) {
-      let novoArrayFizzBuzz =[];
-    for(let i=0;i<arrayFizzBuzz.length;i++){
-        if(arrayFizzBuzz[i]%3===0 && arrayFizzBuzz[i]%5===0){
-            novoArrayFizzBuzz.push("fizzBuzz");
-        }else 
-            if(arrayFizzBuzz[i]%3===0){
-            novoArrayFizzBuzz.push("fizz");
-        }else 
-            if(arrayFizzBuzz[i]%5===0){
-                novoArrayFizzBuzz.push("buzz");
-        }else{
-            novoArrayFizzBuzz.push("bug!");
+  let novoArrayFizzBuzz = [];
+  for (let i = 0; i < arrayFizzBuzz.length; i++) {
+    if (arrayFizzBuzz[i] % 3 === 0 && arrayFizzBuzz[i] % 5 === 0) {
+      novoArrayFizzBuzz.push("fizzBuzz");
+    } else
+      if (arrayFizzBuzz[i] % 3 === 0) {
+        novoArrayFizzBuzz.push("fizz");
+      } else
+        if (arrayFizzBuzz[i] % 5 === 0) {
+          novoArrayFizzBuzz.push("buzz");
+        } else {
+          novoArrayFizzBuzz.push("bug!");
         }
-    }
-    return novoArrayFizzBuzz;
+  }
+  return novoArrayFizzBuzz;
 }
 
 // Desafio 9
 function encode(stringEncode) {
- let newStringEncode ="";
-    for(let i=0;i<stringEncode.length;i++){
-        switch(stringEncode[i]){
-            case "a":
-                newStringEncode=newStringEncode+ "1";
-                break;
-            case "e":
-                newStringEncode=newStringEncode+ "2";
-                break;
-            case "i":
-                newStringEncode=newStringEncode+ "3";
-                break;
-            case "o":
-            newStringEncode=newStringEncode+ "4";
-                break;
-            case "u":
-                newStringEncode=newStringEncode+ "5";
-                break;
-            default:
-                newStringEncode=newStringEncode+ stringEncode[i];
-                break;
-            }   
+  let newStringEncode = "";
+  for (let i = 0; i < stringEncode.length; i++) {
+    switch (stringEncode[i]) {
+      case "a":
+        newStringEncode = newStringEncode + "1";
+        break;
+      case "e":
+        newStringEncode = newStringEncode + "2";
+        break;
+      case "i":
+        newStringEncode = newStringEncode + "3";
+        break;
+      case "o":
+        newStringEncode = newStringEncode + "4";
+        break;
+      case "u":
+        newStringEncode = newStringEncode + "5";
+        break;
+      default:
+        newStringEncode = newStringEncode + stringEncode[i];
+        break;
     }
-    return newStringEncode;
+  }
+  return newStringEncode;
 }
 function decode(stringDecode) {
-  let newStringEncode ="";
-    for(let i=0;i<stringDecode.length;i++){
-        switch(stringDecode[i]){
-            case "1":
-                newStringEncode=newStringEncode+ "a";
-                break;
-            case "2":
-                newStringEncode=newStringEncode+ "e";
-                break;
-            case "3":
-                newStringEncode=newStringEncode+ "i";
-                break;
-            case "4":
-            newStringEncode=newStringEncode+ "o";
-                break;
-            case "5":
-                newStringEncode=newStringEncode+ "u";
-                break;
-            default:
-                newStringEncode=newStringEncode+ stringDecode[i];
-                break;
-            }   
+  let newStringEncode = "";
+  for (let i = 0; i < stringDecode.length; i++) {
+    switch (stringDecode[i]) {
+      case "1":
+        newStringEncode = newStringEncode + "a";
+        break;
+      case "2":
+        newStringEncode = newStringEncode + "e";
+        break;
+      case "3":
+        newStringEncode = newStringEncode + "i";
+        break;
+      case "4":
+        newStringEncode = newStringEncode + "o";
+        break;
+      case "5":
+        newStringEncode = newStringEncode + "u";
+        break;
+      default:
+        newStringEncode = newStringEncode + stringDecode[i];
+        break;
     }
-    return newStringEncode;
+  }
+  return newStringEncode;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,91 +1,91 @@
 // Desafio 10
-function techList(arrayDeTecnologias,name1) {
- arrayDeTecnologias.sort();
+function techList(arrayDeTecnologias, name1) {
+  arrayDeTecnologias.sort();
 
-    let newArrayDeTecnologias =[];
-    if(arrayDeTecnologias.length===0){
+  let newArrayDeTecnologias = [];
+  if (arrayDeTecnologias.length === 0) {
     return "Vazio!"
-    }
-    for(let i=0; i<arrayDeTecnologias.length;i++){
-        newArrayDeTecnologias[i]={
-            tech: arrayDeTecnologias[i],
-            name: name1
-        };  
-    }
-    return newArrayDeTecnologias;
+  }
+  for (let i = 0; i < arrayDeTecnologias.length; i++) {
+    newArrayDeTecnologias[i] = {
+      tech: arrayDeTecnologias[i],
+      name: name1
+    };
+  }
+  return newArrayDeTecnologias;
 }
 
 // Desafio 11
 function generatePhoneNumber(arrayPhoneNumber) {
-  let novoNumero='';
-    if(arrayPhoneNumber.length!=11){
-        return "Array com tamanho incorreto.";
-    }
+  let novoNumero = '';
+  if (arrayPhoneNumber.length != 11) {
+    return "Array com tamanho incorreto.";
+  }
 
-    for(let i=0;i<arrayPhoneNumber.length;i++){
+  for (let i = 0; i < arrayPhoneNumber.length; i++) {
     let repetido = 0
-    if(arrayPhoneNumber[i]<0 || arrayPhoneNumber[i]>9){
-        return "não é possível gerar um número de telefone com esses valores";
+    if (arrayPhoneNumber[i] < 0 || arrayPhoneNumber[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
     }
-    if(i===0){
-        novoNumero= "(" + arrayPhoneNumber[i];
-    }else
-    if(i===1){
-        novoNumero= novoNumero + arrayPhoneNumber[i] +")";
-    }else
-    if(i===2){
-        novoNumero= novoNumero + ' ' + arrayPhoneNumber[i];
-    }else
-    if(i===(arrayPhoneNumber.length-5)){
-        novoNumero= novoNumero + arrayPhoneNumber[i] + '-';
-    }else{
-    novoNumero= novoNumero + arrayPhoneNumber[i];
+    if (i === 0) {
+      novoNumero = "(" + arrayPhoneNumber[i];
+    } else
+      if (i === 1) {
+        novoNumero = novoNumero + arrayPhoneNumber[i] + ")";
+      } else
+        if (i === 2) {
+          novoNumero = novoNumero + ' ' + arrayPhoneNumber[i];
+        } else
+          if (i === (arrayPhoneNumber.length - 5)) {
+            novoNumero = novoNumero + arrayPhoneNumber[i] + '-';
+          } else {
+            novoNumero = novoNumero + arrayPhoneNumber[i];
 
-        }
-    for(let j=0; j<arrayPhoneNumber.length;j++){
-        if(arrayPhoneNumber[j]===arrayPhoneNumber[i]){
-            repetido = repetido+1;
-        }
-        if(repetido>=3){return "não é possível gerar um número de telefone com esses valores";}
+          }
+    for (let j = 0; j < arrayPhoneNumber.length; j++) {
+      if (arrayPhoneNumber[j] === arrayPhoneNumber[i]) {
+        repetido = repetido + 1;
+      }
+      if (repetido >= 3) { return "não é possível gerar um número de telefone com esses valores"; }
     }
-    }
-    return novoNumero;
+  }
+  return novoNumero;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-      let BC = Math.abs(lineB - lineC);
-    let CB = Math.abs(lineC - lineB);
-    let AB = Math.abs(lineA - lineB);
-    let BA = Math.abs(lineB - lineA);
-    let AC = Math.abs(lineA - lineC);
-    let CA = Math.abs(lineC - lineA);
-    
-        if(lineA<lineB+lineC && lineB<lineC+lineA && lineC<lineB+lineA && lineA>BC && lineA>CB && lineB>CA && lineB>AC && lineC>AB && lineC>BA){
-            return true;
-        }else{
-            return false;
-        }
+  let BC = Math.abs(lineB - lineC);
+  let CB = Math.abs(lineC - lineB);
+  let AB = Math.abs(lineA - lineB);
+  let BA = Math.abs(lineB - lineA);
+  let AC = Math.abs(lineA - lineC);
+  let CA = Math.abs(lineC - lineA);
+
+  if (lineA < lineB + lineC && lineB < lineC + lineA && lineC < lineB + lineA && lineA > BC && lineA > CB && lineB > CA && lineB > AC && lineC > AB && lineC > BA) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
 function hydrate(stringHydrate) {
-  let numeroDeCoposAgua = 0 ;
-    for(let i=0;i<stringHydrate.length;i++){
-        for(let j=0;j<=9;j++){
-            let stringJ = '' + j;
-            if(stringHydrate[i]===stringJ){
-                numeroDeCoposAgua = j + numeroDeCoposAgua;
-            }
-        }
+  let numeroDeCoposAgua = 0;
+  for (let i = 0; i < stringHydrate.length; i++) {
+    for (let j = 0; j <= 9; j++) {
+      let stringJ = '' + j;
+      if (stringHydrate[i] === stringJ) {
+        numeroDeCoposAgua = j + numeroDeCoposAgua;
+      }
     }
-    let quantosCopos = '';
-    if(numeroDeCoposAgua===1){
-        quantosCopos = numeroDeCoposAgua + " copo de água";
-    } else{
-        quantosCopos = numeroDeCoposAgua + " copos de água";
-    }
-    return quantosCopos;
+  }
+  let quantosCopos = '';
+  if (numeroDeCoposAgua === 1) {
+    quantosCopos = numeroDeCoposAgua + " copo de água";
+  } else {
+    quantosCopos = numeroDeCoposAgua + " copos de água";
+  }
+  return quantosCopos;
 }
 
 module.exports = {
